4741a3b08e5aab78c8731705cf98ebadf80d2bd0
Mayank Raj <mayraj035@gmail.com>
2024-01-13 17:04:11 +0530

    Initial Commit

---
+++ SnackSwift/SnackSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackSwiftApp.swift
+//  SnackSwift
+//
+//  Created by Mayank Raj on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct SnackSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
